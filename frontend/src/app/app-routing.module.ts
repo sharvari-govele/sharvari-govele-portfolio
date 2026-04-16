18cc4ed00eaa7c0fc1ca402aa53626636810e74d
Sharvari-yy <sharvarigovele777@gmail.com>
2026-04-16 16:14:26 +0530

    wildcard route nont found component display

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './container/notfound/notfound.component';
+import { ContainerComponent } from './container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContainerComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  showNav = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.showNav = event.urlAfterRedirects === '/';
+      });
+  }
+}
